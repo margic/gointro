ed7a27ae2cdfd3ce656ce9805a2d7740f1311ff3
margic <pmcrofts@margic.com>
2017-09-27 09:11:23 -0700

    Adding kafka example

--- simpleserver/main.go
+++ simpleserver/main.go
@@ -8,6 +8,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("localhost:8080", nil)
